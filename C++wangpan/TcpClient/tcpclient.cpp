51408b5b9a256f489879b8f5d31e6a61e30a946f
HuiPuKui <1137438071@qq.com>
2023-04-03 16:11:58 +0800

    create TcpClient

---
+++ C++wangpan/TcpClient/tcpclient.cpp
@@ -0,0 +1,18 @@
+
+#include "tcpclient.h"
+#include "./ui_tcpclient.h"
+
+
+TcpClient::TcpClient(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::TcpClient)
+{
+    ui->setupUi(this);
+}
+
+TcpClient::~TcpClient()
+{
+    delete ui;
+}
+
+
